7347691c0b3ae096f33fa6aae1270a66112aa094
ondrejvankat <ondrej.vankat@peckadesing.cz>
2022-06-11 16:24:52 +0200

    React kalkulačka - připrava funkcí

--- calculator/src/calculator/Result.js
+++ calculator/src/calculator/Result.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Result = () => {
-	return <div className="Result">Výsledek je: 10</div>;
+const Result = (props) => {
+	const result = props.value;
+	if (result || result ===0)
+		return <div className="Result">Výsledek je: {result}</div>;
+	else return null;
 };
 
 export default Result;
